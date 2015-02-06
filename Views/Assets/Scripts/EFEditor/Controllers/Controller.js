a0e6d834e33e974e8e27760cdaaa87c5d741f131
Ellie Harper <elliefops@gmail.com>
2015-02-06 01:07:42 -0500

    Laying out more stuff

--- Views/Assets/Scripts/EFEditor/Controllers/Controller.js
+++ Views/Assets/Scripts/EFEditor/Controllers/Controller.js
@@ -4,5 +4,29 @@ EFEditor.Controllers.Base = (function () {
   {
     self = this;
     self.eventTypes = {};
+
+    self.initMouseEvents = function()
+    {
+      self.eventTypes.Mouse = {
+        CLICK: 'onclick',
+        UP: 'onmouseup',
+        DOWN: 'onmousedown',
+        ENTER: 'onmouseenter',
+        OVER: 'onmouseover',
+        LEAVE: 'onmouseleave',
+        MOVE: 'onmousemove',
+        CONTEXT_MENU: 'oncontextmenu',
+        DOUBLE_CLICK: 'ondblclick'
+      };
+    };
+
+    self.initKeyboardEvents = function()
+    {
+      self.eventTypes.Keyboard = {
+        KEY_DOWN: 'onkeydown',
+        KEY_UP: 'onkeyup',
+        KEY_PRESS: 'onkeypress'
+      };
+    }
   }
 })();
--- Views/Assets/Scripts/EFEditor/Controllers/WPaneController.js
+++ Views/Assets/Scripts/EFEditor/Controllers/WPaneController.js
@@ -1,8 +1,18 @@
 EFEditor.Controllers.WPane = (function(){
+  var self;
   function WPane(node)
   {
-    var self = this;
-    self.handlers = {};
+    self = this;
+    self.prototype = EFEditor.Controllers.Base;
+    function init()
+    {
+      self.eventTypes.INIT_BEFORE  = 'Pre-Initialization';
+      self.eventTypes.INIT_AFTER   = 'Post Initialization';
+      self.eventTypes.PARSE_BEFORE = 'Pre-Parse';
+      self.eventTypes.PARSE_AFTER  = 'Post Parse';
+      self.eventTypes.WPANE_READY  = 'WPane Ready';
+    }
+    init();
   }
 
 
